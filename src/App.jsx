53f1a5c6e4ea37ff34b08536d001a56f52e577ce
manas <ms7215@srmist.edu.in>
2026-08-11 18:55:37 +0530

    Initial VIBR React setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>VIBR 🎵</h1>
+
+      <h2>Find your next obsession.</h2>
+
+      <p>
+        Music discovery without the endless scrolling.
+      </p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
