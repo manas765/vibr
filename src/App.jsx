746b0ff0900a619dda376a61317c8426dbb586dd
Manas YourLastName <your-github-email@example.com>
2026-08-17 01:31:34 +0530

    fix import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import MusicSection from "./components/MusicSection";
 import Collections from "./components/collections";
