abe4ad0fcf9fb1d76790ac6838e5c4d817516e35
manas765 <ms7215@srmist.edu.in>
2026-08-17 16:28:23 +0530

    fix liquid-glass.css location

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./liquid-glass.css";
+import "./components/liquid-glass.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
